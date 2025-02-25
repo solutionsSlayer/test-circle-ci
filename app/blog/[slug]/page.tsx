09acb0b27db0f811b57ba65a00b49ff2dc444bbc
informatique <informatique@gedal.fr>
2025-02-25 16:59:40 +0100

    fIX BUILD

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,14 @@
 import { Metadata } from 'next';
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}): Promise<Metadata> {
-  return { title: `Post: ${params.slug}` };
-}
-
 interface PageProps {
   params: { slug: string };
   searchParams?: { [key: string]: string | string[] | undefined };
+}
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  return { title: `Post: ${params.slug}` };
 }
 
 export default async function Page({ params }: PageProps) {
