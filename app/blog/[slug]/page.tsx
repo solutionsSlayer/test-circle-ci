532afb1da535ed208892657dd081e760bf60ae84
informatique <informatique@gedal.fr>
2025-02-25 16:54:35 +0100

    fix type

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,12 +1,11 @@
-type Props = {
-  params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-};
-
 export async function generateMetadata({ params }: { params: { slug: string } }) {
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: { params: { slug: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: { slug: string };
+}) {
   return <h1>Slug: {params.slug}</h1>;
 }
