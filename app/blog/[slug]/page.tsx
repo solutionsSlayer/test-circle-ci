fa83207310d68894a3846710064dce0dad599e8a
informatique <informatique@gedal.fr>
2025-02-25 16:49:42 +0100

    Test fix build

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,13 @@
-type Params = {
+type Props = {
   params: {
     slug: string;
   };
 };
 
-export async function generateMetadata({ params }: Params) {
+export async function generateMetadata({ params }: Props) {
   return { title: `Post: ${params.slug}` };
 }
 
-export default function Page({ params }: Params) {
+export default function Page({ params }: Props) {
   return <h1>Slug: {params.slug}</h1>;
 }
