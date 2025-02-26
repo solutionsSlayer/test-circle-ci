3a1390cac0288cecdc36e3140ecdfce262a8c2f3
informatique <informatique@gedal.fr>
2025-02-26 07:43:46 +0100

    Test fix build

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,18 @@
 import { Metadata } from 'next';
 
 interface PageProps {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
   searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 export async function generateMetadata({
   params,
 }: PageProps): Promise<Metadata> {
-  return { title: `Post: ${params.slug}` };
+  const resolvedParams = await params;
+  return { title: `Post: ${resolvedParams.slug}` };
 }
 
 export default async function Page({ params }: PageProps) {
-  return <h1>Slug: {params.slug}</h1>;
+  const resolvedParams = await params;
+  return <h1>Slug: {resolvedParams.slug}</h1>;
 }
